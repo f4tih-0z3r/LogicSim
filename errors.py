9cd6b640b3ea5602e4de8bc4db0ffb42952019db
f4tih-0z3r <fatihozer122333@gmail.com>
2021-06-18 22:59:16 +0300

    Debug class, printing debug function, some comments

--- errors.py
+++ errors.py
@@ -1,6 +1,11 @@
 class invalidDebugLevelError(Exception):
     def __init__(self):
-        self.msg = "debugLevel must in rannge of (1, 4)"
+        self.msg = "Debug level must in rannge of (1, 4)"
+        super().__init__(self.msg)
+
+class invalidDebugMsgError(Exception):
+    def __init__(self):
+        self.msg = "Message must be str"
         super().__init__(self.msg)
 
 class invalidScreenSizeError(Exception):
--- main.py
+++ main.py
@@ -1,10 +1,50 @@
 import errors
+from termcolor import colored
 
 def main():
+    class debugClass:
+        def __init__(self):
+            pass
+
+        def printDebug(self, debugLevel, msg):
+            """
+            debugLevel = 1: info
+            debugLevel = 2: warning
+            debugLevel = 3: important warning
+            debugLevel = 4: error
+            """
+
+            #Checking validity of debugLevel
+            if type(debugLevel) == int:
+                if not (debugLevel <= 4 and debugLevel >= 1):
+                    raise errors.invalidDebugLevelError()
+            else:
+                raise errors.invalidDebugLevelError()
+
+            #When debugLevel is valid
+            #Checking validity of msg
+            if type(msg) != str:
+                raise invalidDebugMsgError()
+
+            #When msg is valid
+            if debugLevel == 1: #Info
+                print(msg)
+            elif debugLevel == 2: #Warning
+                print(colored(msg, "yellow"))
+            elif debugLevel == 3: #Important warning
+                print(colored(msg, "red"))
+            elif debugLevel == 4: #Error
+                print(colored(msg, "white", "on_red"))
+
+
     class applClass:
         def __init__(self, screenSize = None):
+            #When screenSize not entered manually
             if screenSize == None:
                 self.screenSize = (1200, 600)
+
+            #When screenSize entered manually
+            #Checking validity of screenSize
             else:
                 if type(screenSize) == tuple:
                     if len(screenSize) == 2:
@@ -22,9 +62,8 @@ def main():
                 else:
                     raise errors.invalidScreenSizeError()
 
+                #When screenSize is valid
                 self.screenSize = screenSize
-
-    appl = applClass((1200, 600))
 
 if __name__ == "__main__":
     main()
